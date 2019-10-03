aa1c9cba59c0644c10e841a21fb512ef985d1f5a
jarednrice <jarednrice@gmail.com>
2019-10-03 17:38:03 -0400

    created header file for jet class

--- Jet.h
+++ Jet.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+	string myEngineSize;
+	int myNumberOfEngines;
+
+public:
+	Jet();
+
+	explicit Jet(string brand, string model, string fuelType, 
+			string engineSize, int numberOfEngines);
+	virtual ~Jet();
+	string getEngineSize();
+	void setEngineSize(string engineSize);
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
