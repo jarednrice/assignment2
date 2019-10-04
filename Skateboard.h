a117c0782e48f5000a13483eb0aeadde20c1cc8c
jarednrice <jarednrice@gmail.com>
2019-10-03 21:37:12 -0400

    created functioning skateboard class, further testing needed

--- Skateboard.h
+++ Skateboard.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle {
+
+private:
+
+public:
+	explicit Skateboard(string brand, string model);
+	virtual ~Skateboard();
+	double random_doubles(double min, double max);
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_SKATEBOARD_H
